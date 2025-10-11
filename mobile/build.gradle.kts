fca1a6fc38f2c9a566fb377be55529356720b6b4
xyugen <alex.arias.jorym@gmail.com>
2025-10-11 14:05:53 +0800

    chore: bump version to 1.1.0

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pagzone.sonavi"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pagzone.sonavi"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.1.0"
 
     }
 
